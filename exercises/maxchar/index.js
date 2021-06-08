a08c76fdbfffc19417d907bb3ced01c0dc02b235
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-06-08 20:58:47 +0700

    Max Character, I love it !! 🌻

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {}
+  let max = 0
+  let maxChar = ''
+
+  for (const char of str) {
+    if (!chars[char]) {
+      chars[char] = 1
+    } else {
+      chars[char]++
+    }
+  }
+
+  for (const key in chars) {
+    if (chars[key] > max) {
+      max = chars[key]
+      maxChar = key
+    }
+  }
+
+  return maxChar
+}
 
 module.exports = maxChar;
