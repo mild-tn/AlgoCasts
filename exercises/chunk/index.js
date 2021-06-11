03670aa7ecd6c6da90e1a366e8f6677f99a9c9f7
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-06-11 14:55:58 +0700

    Chunked 2 🌻

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,17 +10,29 @@
 
 function chunk(array, size) {
   let chunked = []
+  let index = 0
 
-  for (let element of array) {
-    const last = chunked[chunked.length - 1]
-
-    if (!last || last.length === size) {
-      chunked.push([element])
-    } else {
-      last.push(element)
-    }
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size;
   }
+
   return chunked
 }
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   let chunked = []
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1]
+
+//     if (!last || last.length === size) {
+//       chunked.push([element])
+//     } else {
+//       last.push(element)
+//     }
+//   }
+//   return chunked
+// }
