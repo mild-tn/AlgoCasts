72e694a6b20dc855ff5312904b8afbb8273e59e9
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-06-11 19:41:48 +0700

    :beers: Anagrams [I love it agian!!] 🌻

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -6,8 +6,51 @@
 // --- Examples
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
-//   anagrams('Hi there', 'Bye there') --> False
+//   anagrams('Hi there', 'Bye there') ---> False
 
-function anagrams(stringA, stringB) {}
+// Method 2
+function anagrams(stringA, stringB) {
+ return cleanString(stringA) === cleanString(stringB)
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
 
 module.exports = anagrams;
+
+// Method 1
+// function anagrams(stringA, stringB) {
+//   const strMapA = buildCharMap(stringA)
+//   const strMapB = buildCharMap(stringB)
+  
+//   if (Object.keys(strMapA).length !== Object.keys(strMapB).length) {
+//     return false
+//   }
+  
+//   for (let char in strMapA) {
+//     // check count of character if not equal return false.
+//     if (strMapA[char] !== strMapB[char]) {
+//       return false
+//     }
+//   }
+  
+//   return true
+// }
+
+// function buildCharMap(str) {
+//   const cleanStr = str.replace(/[^\w]/g, '').toLowerCase()
+//   const charMap = {}
+
+//   for (const char of cleanStr) {
+//     if (!cleanStr[char]) {
+//       charMap[char] = 1
+//     } else {
+//       charMap[char]++
+//     }
+//     // or you can use this instead above.
+//     // charMap[char] = charMap[char] + 1 || 1
+//   }
+
+//   return charMap
+// }
