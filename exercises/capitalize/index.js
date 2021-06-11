d4588ab54e87b4551b43516ef0c2614235415e9d
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-06-11 20:47:06 +0700

    :beers: Capitalize ~~~~ 🌻

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Method 2
+function capitalize(str) {
+  let words = []
+
+  for (let word of str.split(' ')) {
+    words = [...words, word[0].toUpperCase() + word.slice(1)]
+  }
+
+  return words.join(' ')
+}
 
 module.exports = capitalize;
+
+// Method 1
+// function capitalize(str) {
+//   const words = str.split(' ')
+//   let arrayWord = []
+
+//   for (const word of words) {
+//     let firstLetterUpperCase = word.replace(word[0], word.slice(0, 1).toUpperCase())
+//     arrayWord = [...arrayWord, firstLetterUpperCase]
+//   }
+
+//   return arrayWord.join(' ')
+// }
