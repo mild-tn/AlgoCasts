b184c8b0104f173f6d047dccc5fbdc2c08d8697b
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-06-12 23:38:27 +0700

    Find vowels 🌻

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,39 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
 
 module.exports = vowels;
+
+// Method 1
+// function vowels(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u']
+//   let countVowels = 0
+
+//   for (let i = 0; i < vowels.length; i++) {
+//     for (const char of str) {
+//       if (char.toLowerCase() === vowels[i]) {
+//         countVowels = countVowels + 1
+//       }
+//     }
+//   }
+
+//   return countVowels
+// }
+
+// Method 2
+// function vowels(str) {
+//   let countVowels = 0
+//   let checker = 'aeiou'
+
+//   for (const char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       countVowels++
+//     }
+//   }
+
+//   return countVowels
+// }
