89a13704f7e40fb731e7685b0737327a237f761b
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-06-13 22:22:04 +0700

    Stack class 🌻

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,23 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.data = []
+  }
+
+  push(item) {
+    this.data = [...this.data, item]
+    return this.data
+  }
+
+  pop() {
+    return this.data.pop()
+  }
+
+  peek() {
+    return this.data[this.data.length - 1]
+  }
+}
 
 module.exports = Stack;
